6ad718636c48128b3a4bbbb6d7deb26d299f975e
justin-theodorus <justin.theodorus@gmail.com>
2025-12-28 17:00:52 +0700

    modify frontend types

--- frontend/src/types/models.ts
+++ frontend/src/types/models.ts
@@ -1,38 +1,36 @@
-// User, Topic, Post, Comment
-
 export interface User {
-    id: number
-    username: string
-    email: string
-    createdAt: string
+  id: number
+  username: string
+  email: string
+  createdAt: string
 }
 export interface Topic {
-    id: number
-    title: string
-    description: string
-    userId: number
-    user: User
-    createdAt: string
-    updatedAt: string
-    postsCount?: number
+  id: number
+  title: string
+  description: string
+  userId: number
+  user: User
+  createdAt: string
+  updatedAt: string
+  postsCount?: number
 }
 export interface Post {
-    id: number
-    topicId: number
-    userId: number
-    title: string
-    content: string
-    user: User
-    createdAt: string
-    updatedAt: string
-    commentsCount?: number // can be none
+  id: number
+  topicId: number
+  userId: number
+  title: string
+  content: string
+  user: User
+  createdAt: string
+  updatedAt: string
+  commentsCount?: number
 }
 export interface Comment {
-    id: number
-    postId: number
-    userId: number
-    content: string
-    user: User
-    createdAt: string
-    updatedAt: string
+  id: number
+  postId: number
+  userId: number
+  content: string
+  user: User
+  createdAt: string
+  updatedAt: string
 }
